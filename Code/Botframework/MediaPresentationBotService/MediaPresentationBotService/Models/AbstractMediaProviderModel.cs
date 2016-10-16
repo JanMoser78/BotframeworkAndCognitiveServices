b25b505df7a39dbad186da0322c19172c93b0234
Jan Moser <jan.moser@gmx.ch>
2016-10-16 13:29:18 +0200

    added abstract model class as base for the specific implementations

---
+++ Code/Botframework/MediaPresentationBotService/MediaPresentationBotService/Models/AbstractMediaProviderModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MediaPresentationBotService.Models
+{
+    public abstract class AbstractMediaProviderModel
+    {
+        public virtual async Task ConnectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task DisconnectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public virtual async Task RetrieveData()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
